f44ac19f97c42d8e70c7a96fa5955f96dd6f4cca
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-12-04 19:57:22 -0600

    3rd 15-1

--- MVC/15/15-1/15-1-1/server.js
+++ MVC/15/15-1/15-1-1/server.js
@@ -4,6 +4,8 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const carsForSale = require('./models/carsForSale.json');
+
 app.use(express.static('public'));
 
 app.use(express.json());
@@ -12,7 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 const hbs = exphbs.create({});
 
 app.get("/", (req, res) => {
+  const lastCarObj = carsForSale[carsForSale.length - 1];
+  return res.render("cars", { shopTitle: shopTitle, carsForSale, lastCar });
+});
 
+app.get("/cars", (req, res) => {
   return res.render("index", { msg: "Hello World!" });
 });
 
@@ -22,3 +28,4 @@ app.set('view engine', 'handlebars');
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
+
