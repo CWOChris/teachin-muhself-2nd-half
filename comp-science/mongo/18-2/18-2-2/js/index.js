3b232040f9cedbfdfbc52a09366563fe88bd49eb
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-01-11 19:34:59 -0600

    find stuff and update it with CRUD in Mongoose

---
+++ comp-science/mongo/18-2/18-2-2/js/index.js
@@ -0,0 +1,9 @@
+// Mongoose CRUD Read
+
+.find() // returns all documents
+.findOne() // returns one document
+.findById() // returns one document by id
+
+// Mongoose CRUD Delete
+findOneAndDelete() // deletes one document
+findByIdAndDelete() // deletes one document by id
