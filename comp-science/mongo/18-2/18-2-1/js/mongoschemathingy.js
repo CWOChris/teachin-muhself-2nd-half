94bf184f4c767c8c5f65a976402b3eb04f895e8e
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-01-11 19:24:20 -0600

    added a destructuring example to 18-2-1"

---
+++ comp-science/mongo/18-2/18-2-1/js/mongoschemathingy.js
@@ -0,0 +1,13 @@
+function name({first}, company) {
+    console.log("first", first);
+}
+
+const person = {
+    first: "Jane",
+    last: "Doe",
+    company: "ABC Inc."
+};
+
+const company = "XYZ Inc.";
+
+name(person, company);
