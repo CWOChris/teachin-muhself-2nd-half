e16dc592139ef8aad874c6f12a545db523d92a46
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-01-11 19:35:31 -0600

    update to comments

--- comp-science/mongo/18-2/18-2-2/js/index.js
+++ comp-science/mongo/18-2/18-2-2/js/index.js
@@ -1,9 +1,9 @@
 // Mongoose CRUD Read
 
-.find() // returns all documents
-.findOne() // returns one document
-.findById() // returns one document by id
+// .find() // returns all documents
+// .findOne() // returns one document
+// .findById() // returns one document by id
 
-// Mongoose CRUD Delete
-findOneAndDelete() // deletes one document
-findByIdAndDelete() // deletes one document by id
+// // Mongoose CRUD Delete
+// findOneAndDelete() // deletes one document
+// findByIdAndDelete() // deletes one document by id
