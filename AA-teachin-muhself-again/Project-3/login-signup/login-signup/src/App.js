d6206176ff12e07cdd1a67ebe104cdeb0bdf95c1
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-20 21:30:39 -0600

    finished form

---
+++ AA-teachin-muhself-again/Project-3/login-signup/login-signup/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignUp from './components/login-signup/LoginSignUp';
+
+function App() {
+  return (
+    <div>
+      <LoginSignUp />
+    </div>
+  );
+}
+
+export default App;
